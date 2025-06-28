b4864eec75c1b4f1c912fde7a61ebde095c62733
Frs <sergeq198.97@yandex.ru>
2025-06-28 15:01:07 +0700

    Revert "Reapply "test""
    This reverts commit e7e693940e753e5133a6180e7569ba457f7de3ca.

--- OtusMiniApi/Program.cs
+++ OtusMiniApi/Program.cs
@@ -5,7 +5,7 @@ using Application;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var test = "test version two";
+var test = "test version one";
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
