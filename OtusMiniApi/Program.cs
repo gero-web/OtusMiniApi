884194879fe7cf027cc2bde47d32d89f8e5e218f
Frs <sergeq198.97@yandex.ru>
2025-06-28 16:36:19 +0700

    g

--- OtusMiniApi/Program.cs
+++ OtusMiniApi/Program.cs
@@ -4,8 +4,8 @@ using Application;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var test = "h";
+ 
+var test = "h"; 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
