6e6dc7f06fcaeb7dc6196bff5ae9cb77e0b9f278
Frs <sergeq198.97@yandex.ru>
2025-07-06 09:47:07 +0700

    добавлены метрики

--- OtusMiniApi/Program.cs
+++ OtusMiniApi/Program.cs
@@ -1,11 +1,12 @@
 
 
 using Application;
+using Microsoft.Extensions.Options;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var test = "test version one";
+// Add services to the container. 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -13,8 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Logging.AddConsole();
 
 builder.Services.AddApplicationDI();
-
 var app = builder.Build();
+app.UseRouting();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,10 +24,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpMetrics(options =>
+{
+    options.ReduceStatusCodeCardinality();
+    options.AddCustomLabel("host", context => context.Request.Host.Host);
+});
+app.UseMetricServer();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseEndpoints(end =>
+    {
+        _ = end.MapMetrics(pattern: "/sergey/prometheus");
+    }
+
+    );
 
 app.Run();
